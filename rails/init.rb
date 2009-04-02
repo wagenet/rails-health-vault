1c0afee62b5a528b66a7cb0c23bf406ce76cf0cd
Peter Wagenet <peter.wagenet@gmail.com>
2009-04-02 13:44:13 -0400

    Further Rails prep

---
+++ rails/init.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'active_support'
+
+HEALTHVAULT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+$LOAD_PATH << File.join(HEALTHVAULT_ROOT, 'lib')
+$LOAD_PATH << File.join(HEALTHVAULT_ROOT, 'lib', 'generated')
+
+Dependencies = ActiveSupport::Dependencies unless defined?(Dependencies)
+Dependencies.load_paths << File.join(HEALTHVAULT_ROOT, 'lib')
+Dependencies.load_paths << File.join(HEALTHVAULT_ROOT, 'lib', 'generated')
+
+require 'rexml/formatters/pretty_fix'
