e594b0c04e47d834b47fe9fdb4b80adbd7056ee3
Peter Wagenet <peter.wagenet@gmail.com>
2009-06-09 20:14:00 -0700

    Copy HV certs to Rails config

--- install.rb
+++ install.rb
@@ -5,5 +5,8 @@ RAILS_ROOT = "#{HEALTHVAULT_ROOT}/../../.." unless defined?(RAILS_ROOT)
 puts "Copying config to config/healthvault.yml"
 `cp #{HEALTHVAULT_ROOT}/templates/healthvault.yml #{RAILS_ROOT}/config/healthvault.yml`
 
+puts "Copying certs to config/healthvault.pem"
+`cp #{HEALTHVAULT_ROOT}/bin/certs/helloWorld.pem #{RAILS_ROOT}/config/healthvault.pem`
+
 puts "Generating Models"
 HealthVault::CodeGeneration::Generator.get_things
